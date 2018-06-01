cdb7f1b0c9bb94b0f13036d244a8b103e4f7af2f
kl <aadian@163.com>
2018-06-01 17:50:52 +0800

    Merge branch 'master' of https://github.com/aadian/spark23
    # Conflicts:
    #	src/main/scala/com/kk/ln/lab/DFJoin.scala

--- src/main/scala/com/kk/ln/databricks/StructuredStreamETL/ETL.scala
+++ src/main/scala/com/kk/ln/databricks/StructuredStreamETL/ETL.scala
@@ -1,5 +1,92 @@
 package com.kk.ln.databricks.StructuredStreamETL
 
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{ArrayType, StringType, StructType}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.streaming.{ProcessingTime, Trigger}
+
 object ETL {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().master("local[2]").appName("ETL").getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+    import spark.implicits._
+
+    /**
+      * schema:
+      * Records
+      * userid: 001
+      * username: Jim Zhang
+      * loginTimes 1527830168
+      *
+      * userid: 002
+      * *   username: Rose Liu
+      * *   loginTimes 1527830168
+      * ...
+      */
+    //    val logSchema = new StructType()
+    //      .add("Records", ArrayType(new StructType()
+    //        .add("userId", StringType)
+    //        .add("userName", new StructType()
+    //          .add("firstName", StringType)
+    //          .add("lastName", StringType))
+    //        .add("loginTime", StringType)))
+    val logSchema = new StructType()
+      .add("Records", ArrayType(new StructType()
+        .add("userId", StringType)
+        .add("userName", new StructType()
+          .add("firstName", StringType)
+          .add("lastName", StringType))
+        .add("loginTime", StringType)
+      ))
+
+
+    val logPath = "E:\\spark\\spark23\\src\\main\\resources\\logs\\"
+    val parquetOutputPath = "E:\\spark\\spark23\\src\\main\\resources\\cloudtrail"
+    val checkPointPath = "E:\\spark\\spark23\\src\\main\\resources\\checkpoint"
+
+    val rawRecords = spark.readStream
+      .option("maxFilesPerTrigger", "100")
+      .schema(logSchema)
+      .json(logPath)
+
+    //    val cloudTrailEvents = rawRecords
+    //      .select(explode($"Records") as "record")
+    //      .select(
+    //        unix_timestamp($"record.loginTime", "yyyy-MM-dd'T'hh:mm:ss").cast("timestamp") as "loginTime",
+    //        $"record.userName")
+
+    val cloudTrailEvents = rawRecords
+      .select(explode($"Records") as "record")
+      .select(
+        $"record.userName",
+        unix_timestamp($"record.loginTime", "yyyy-MM-dd hh:mm:ss").cast("timestamp") as "loginTime"
+      )
+
+    //.select(unix_timestamp($"record.loginTime", "yyyy-MM-dd'T'hh:mm:ss").cast("timestamp") as "loginTime", "record.userName")
+
+    //
+    //    val checkPointPath = "E:\\spark\\spark23\\src\\main\\resources\\checkpoint"
+    //    val streamingELTQuery = cloudTrailEvents
+    //      .writeStream
+    //      .format("console")
+    //      .outputMode("append")
+    //      .start()
+
+
+    val streamingELTQuery = cloudTrailEvents.withColumn("date", $"loginTime".cast("date"))
+      .writeStream
+      .format("parquet")
+      .option("path", parquetOutputPath)
+      .partitionBy("date")
+      .trigger(Trigger.ProcessingTime("10 seconds"))
+      .option("checkpointLocation", checkPointPath)
+      .start()
+
+
+    streamingELTQuery.awaitTermination()
+
+
+  }
 
 }
--- src/main/scala/com/kk/ln/databricks/StructuredStreamETL/Query.scala
+++ src/main/scala/com/kk/ln/databricks/StructuredStreamETL/Query.scala
@@ -1,94 +1,20 @@
 package com.kk.ln.databricks.StructuredStreamETL
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.{ArrayType, StringType, StructType}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.ProcessingTime
+import org.apache.spark.sql.types.{ArrayType, StringType, StructType}
 
-object ETL {
+object Query {
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().master("local[2]").appName("ETL").getOrCreate()
     spark.sparkContext.setLogLevel("WARN")
     import spark.implicits._
 
-    //    val parquetOutputPath = "E:\\spark\\spark23\\src\\main\\resources\\cloudtrail"
-    //    val parquetData = spark.sql(s"select userName from parquet.`$parquetOutputPath`")
-    //    parquetData.show()
-
-    /**
-      * schema:
-      * Records
-      * userid: 001
-      * username: Jim Zhang
-      * loginTimes 1527830168
-      *
-      * userid: 002
-      * *   username: Rose Liu
-      * *   loginTimes 1527830168
-      * ...
-      */
-    //    val logSchema = new StructType()
-    //      .add("Records", ArrayType(new StructType()
-    //        .add("userId", StringType)
-    //        .add("userName", new StructType()
-    //          .add("firstName", StringType)
-    //          .add("lastName", StringType))
-    //        .add("loginTime", StringType)))
-    val logSchema = new StructType()
-      .add("Records", ArrayType(new StructType()
-        .add("userId", StringType)
-        .add("userName", new StructType()
-          .add("firstName", StringType)
-          .add("lastName", StringType))
-        .add("loginTime", StringType)
-      ))
-
-
-    val logPath = "E:\\spark\\spark23\\src\\main\\resources\\logs\\"
     val parquetOutputPath = "E:\\spark\\spark23\\src\\main\\resources\\cloudtrail"
-    val checkPointPath = "E:\\spark\\spark23\\src\\main\\resources\\checkpoint"
-
-    val rawRecords = spark.readStream
-      .option("maxFilesPerTrigger", "100")
-      .schema(logSchema)
-      .json(logPath)
-
-    //    val cloudTrailEvents = rawRecords
-    //      .select(explode($"Records") as "record")
-    //      .select(
-    //        unix_timestamp($"record.loginTime", "yyyy-MM-dd'T'hh:mm:ss").cast("timestamp") as "loginTime",
-    //        $"record.userName")
-
-    val cloudTrailEvents = rawRecords
-      .select(explode($"Records") as "record")
-      .select(
-        $"record.userName",
-        unix_timestamp($"record.loginTime", "yyyy-MM-dd hh:mm:ss").cast("timestamp") as "loginTime"
-      )
-
-    //.select(unix_timestamp($"record.loginTime", "yyyy-MM-dd'T'hh:mm:ss").cast("timestamp") as "loginTime", "record.userName")
-
-    //
-    //    val checkPointPath = "E:\\spark\\spark23\\src\\main\\resources\\checkpoint"
-    //    val streamingELTQuery = cloudTrailEvents
-    //      .writeStream
-    //      .format("console")
-    //      .outputMode("append")
-    //      .start()
-
-
-    val streamingELTQuery = cloudTrailEvents.withColumn("date", $"loginTime".cast("date"))
-      .writeStream
-      .format("parquet")
-      .option("path", parquetOutputPath)
-      .partitionBy("date")
-      .trigger(ProcessingTime("10 seconds"))
-      .option("checkpointLocation", checkPointPath)
-      .start()
-
-
-    streamingELTQuery.awaitTermination()
+    val parquetData = spark.sql(s"select * from parquet.`$parquetOutputPath`")
+    parquetData.show()
 
 
   }
