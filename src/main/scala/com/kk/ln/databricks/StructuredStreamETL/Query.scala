356281f969ceff7b35b5f09c22626de6fe6ad826
kl <aadian@163.com>
2018-09-03 08:40:15 +0800

    Merge branch 'master' of https://github.com/aadian/spark23
    # Conflicts:
    #	src/main/scala/com/kk/ln/lab/DFJoin.scala

--- src/main/scala/com/kk/ln/databricks/StructuredStreamETL/Query.scala
+++ src/main/scala/com/kk/ln/databricks/StructuredStreamETL/Query.scala
@@ -1,19 +1,15 @@
 package com.kk.ln.databricks.StructuredStreamETL
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.streaming.ProcessingTime
-import org.apache.spark.sql.types.{ArrayType, StringType, StructType}
 
 object Query {
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().master("local[2]").appName("ETL").getOrCreate()
     spark.sparkContext.setLogLevel("WARN")
-    import spark.implicits._
 
     val parquetOutputPath = "E:\\spark\\spark23\\src\\main\\resources\\cloudtrail"
-    val parquetData = spark.sql(s"select * from parquet.`$parquetOutputPath`")
+    val parquetData = spark.sql(s"select userName, count(*), row_number() OVER (PARTITION BY userName order by count(*)) rank from parquet.`$parquetOutputPath` group by userName")
     parquetData.show()
 
 
